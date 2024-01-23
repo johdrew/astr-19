1fd0cce6eb1bd9d45e7d1a28867620782605bff5
johdrew <156861671+johdrew@users.noreply.github.com>
2024-01-22 23:10:06 -0800

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,18 @@
+def main():
+    name = 'John Henry Drew'
+    pronouns = 'he/him'
+    fav_movie = 'Chasing Mavericks'
+    fav_food = 'poke'
+
+    print('name:', name)
+
+    if pronouns:
+        print('pronouns:', pronouns)
+
+    print('fav_movie:', fav_movie)
+    print('fav_food:', fav_food)
+
+if __name__ == '__main__':
+    main()
+
+

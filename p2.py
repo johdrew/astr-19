83379355919b5d88dc754f9a3a86e187c0f786aa
johdrew <156861671+johdrew@users.noreply.github.com>
2024-01-22 23:59:23 -0800

    Add files via upload
    prompt 2

---
+++ p2.py
@@ -0,0 +1,25 @@
+def main():
+	#floating nums
+	FloatNum1 = 2.4
+	FloatNum2 = 3.4
+	#integers
+	Int1 = 4
+	Int2 = 5
+
+	#sum functionn
+	sum_result = FloatNum1 + FloatNum2
+	print_result('Sum,', sum_result)
+
+	#diff function
+	diff_result = Int1 - Int2
+	print_result('Difference,', diff_result)
+
+	#product function
+	product_result = FloatNum1 * Int1
+	print_result('Product,', product_result)
+
+def print_result(operation, result):
+	print(f'{operation}: {result}, Type: {type(result)}')
+
+if __name__ == '__main__':
+	main()

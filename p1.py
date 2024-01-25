6f5f35c6a88158d567ed516b25796e87bd8ba31f
johdrew <156861671+johdrew@users.noreply.github.com>
2024-01-24 16:47:54 -0800

    Add files via upload
    prompt #1

---
+++ p1.py
@@ -0,0 +1,14 @@
+def main():
+    name = 'John Henry Drew'
+    pronouns = 'he/him'
+    About_Me = 'My favorite movie is Chasing Mavericks, and it is a big part of the reason that I decided to come to Santa Cruz. My favorite food is Poke and I enjoy making it.'
+
+    print('name:', name)
+
+    if pronouns:
+        print('pronouns:', pronouns)
+
+    print('About me:', About_Me)
+
+if __name__ == '__main__':
+    main()
